c97ef9faa55756e40c722bf80f7ab949752a93d6
Camilo Velez <camilov61@gmail.com>
2023-04-10 20:57:55 -0400

    added G

--- Project_2/hw1.jl
+++ Project_2/hw1.jl
@@ -2,7 +2,7 @@ using DelimitedFiles
 using LinearAlgebra
 
 (R, num_vert_) = readdlm("Project_2/kn57Nodes1to57_dist.txt", Float64, header=true);
-num_vert = parse(Float64, num_vert_[1]);
+num_vert = parse(Float64, num_vert_[1])
 # S = zeros(size(R))
 # rho = 0;
 # for i = 1:57, j = 1:57
@@ -12,8 +12,14 @@ num_vert = parse(Float64, num_vert_[1]);
 # end
 S = R .^ 2
 rho = norm(S, 1)
-rho \= (2 * num_vert);
-one_col = ones(57, 1);
-v = (S - rho * I) * one_col / num_vert;
+rho \= (2 * num_vert)
+one_col = ones(57, 1)
+v = (S - rho * I) * one_col / num_vert
+function G(n::Real, S::Matrix, rho::Real)::Matrix
+    r = 1 / 2n * (S - rho * I) * 1 * 1' + 1 / 2n * 1 * 1' * (S - rho * I) - 1 / 2 * S
+    @assert issymmetric(r)
+    return r
+end
+gmatrix = G(num_vert, S, rho)
 
 
