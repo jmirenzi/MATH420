7009c0e183e5e11039a818a8b2c6117fad6dee9c
Camilo Velez <camilov61@gmail.com>
2023-04-10 20:44:10 -0400

    compacted code

--- Project_2/hw1.jl
+++ Project_2/hw1.jl
@@ -1,17 +1,19 @@
 using DelimitedFiles
 using LinearAlgebra
 
-(R, num_vert_) = readdlm("Project_2/kn57Nodes1to57_dist.txt",Float32,header=true);
-num_vert = parse(Float32,num_vert_[1]);
-S=zeros(size(R))
-rho=0;
-for i=1:57, j=1:57
-    global S,R,rho;
-    S[i,j] = R[i,j]^2;
-    rho += S[i,j];
-end
-rho\=(2*num_vert);
-one_col = ones(57,1);
-v = (S-rho*I)*one_col/num_vert;
+(R, num_vert_) = readdlm("Project_2/kn57Nodes1to57_dist.txt", Float64, header=true);
+num_vert = parse(Float64, num_vert_[1]);
+# S = zeros(size(R))
+# rho = 0;
+# for i = 1:57, j = 1:57
+#     global S, R, rho
+#     S[i, j] = R[i, j]^2
+#     rho += S[i, j]
+# end
+S = R .^ 2
+rho = norm(S, 1)
+rho \= (2 * num_vert);
+one_col = ones(57, 1);
+v = (S - rho * I) * one_col / num_vert;
 
 
