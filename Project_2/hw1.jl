ee1bed8f9ab339f129a58be2cfaab592a33fdefc
Camilo Velez <camilov61@gmail.com>
2023-04-18 01:01:39 -0400

    Co-authored-by: Jack Mirenzi <jmirenzi@users.noreply.github.com>

--- Project_2/hw1.jl
+++ Project_2/hw1.jl
@@ -3,16 +3,13 @@ using LinearAlgebra
 using Plots
 using LaTeXStrings
 
+export GHW1
+
+GHW1 = Dict{String,Any}("exactdist" => 0, "dist" => 0)
 for name = ("dist", "exactdist")
     (R, num_vert_) = readdlm("Project_2/kn57Nodes1to57_" * name * ".txt", Float64, header=true)
     num_vert = parse(Float64, num_vert_[1])
-    # S = zeros(size(R))
-    # rho = 0;
-    # for i = 1:57, j = 1:57
-    #     global S, R, rho
-    #     S[i, j] = R[i, j]^2
-    #     rho += S[i, j]
-    # end
+
     S = R .^ 2
 
     one_col = ones(57, 1)
@@ -25,41 +22,42 @@ for name = ("dist", "exactdist")
         @assert issymmetric(r)
         return r
     end
-    G = getGram(num_vert, S, rho)
-    ev, Q = eigen(G, sortby=x -> -x)
-    index = findfirst(x -> x < 0, ev)
-    ev[index-1:end] .= 0
-    display(scatter(ev, labels=nothing, title=L"Eigenvalues \quad for \quad G for \quad %$(name)", xaxis=L"Edges", yaxis=L"Value"))
-    print("The 10 largest eigenvalues are $(first(ev, 10))")
-    Λ = Diagonal(ev)
-    lambda = Λ
-    Q[:, 1:2]
-
-    Y::Dict{Int,Matrix} = Dict([])
-    R_hat::Dict{Int,Matrix} = Dict([])
-    R_norm::Dict{Int,Real} = Dict([])
-    ϵ::Dict{Int,Real} = Dict([])
-    σ::Dict{Int,Real} = Dict([])
-
-    println("For data " * name)
-    for d = (2, 3)
-        println("d = $(d)")
-        Q_1 = Q[:, 1:d]
-        Λ_1 = Λ[1:d, 1:d]
-        Y[d] = Λ_1^1 / 2 * Q_1'
-        R_hat[d] = zeros(57, 57)
-        for i = 1:57, j = 1:57
-            R_hat[d][i, j] = norm(Y[d][1:d, j] - Y[d][1:d, i])
-        end
-        R_norm[d] = norm(R - R_hat[d])
-        println("Norm = $(R_norm[d])")
-        ϵ[d] = norm(G - Y[d]'Y[d])
-        println("ϵ = $(ϵ[d])")
-        σ[d] = sqrt(sum(ev[d+1:end] .^ 2))
-        println("σ = $(σ[d])")
-        println("Difference = $(ϵ[d] - σ[d])")
-    end
-    display(scatter(Y[2][1, :], Y[2][2, :], title="Two dimensional Embedding for " * name))
-    display(scatter3d(Y[3][1, :], Y[3][2, :], Y[3][3, :], title="Three dimensional Embedding for " * name))
-    # scatter3d(Y[3][1,:], Y[3][2,:], Y[3][3,:], camera=[0,0,0])
+    GHW1[name] = getGram(num_vert, S, rho)
 end
+#     ev, Q = eigen(G, sortby=x -> -x)
+#     index = findfirst(x -> x < 0, ev)
+#     ev[index-1:end] .= 0
+#     display(scatter(ev, labels=nothing, title=L"Eigenvalues \quad for \quad G for \quad %$(name)", xaxis=L"Edges", yaxis=L"Value"))
+#     print("The 10 largest eigenvalues are $(first(ev, 10))")
+#     Λ = Diagonal(ev)
+#     lambda = Λ
+#     Q[:, 1:2]
+
+#     Y::Dict{Int,Matrix} = Dict([])
+#     R_hat::Dict{Int,Matrix} = Dict([])
+#     R_norm::Dict{Int,Real} = Dict([])
+#     ϵ::Dict{Int,Real} = Dict([])
+#     σ::Dict{Int,Real} = Dict([])
+
+#     println("For data " * name)
+#     for d = (2, 3)
+#         println("d = $(d)")
+#         Q_1 = Q[:, 1:d]
+#         Λ_1 = Λ[1:d, 1:d]
+#         Y[d] = Λ_1^1 / 2 * Q_1'
+#         R_hat[d] = zeros(57, 57)
+#         for i = 1:57, j = 1:57
+#             R_hat[d][i, j] = norm(Y[d][1:d, j] - Y[d][1:d, i])
+#         end
+#         R_norm[d] = norm(R - R_hat[d])
+#         println("Norm = $(R_norm[d])")
+#         ϵ[d] = norm(G - Y[d]'Y[d])
+#         println("ϵ = $(ϵ[d])")
+#         σ[d] = sqrt(sum(ev[d+1:end] .^ 2))
+#         println("σ = $(σ[d])")
+#         println("Difference = $(ϵ[d] - σ[d])")
+#     end
+#     display(scatter(Y[2][1, :], Y[2][2, :], title="Two dimensional Embedding for " * name))
+#     display(scatter3d(Y[3][1, :], Y[3][2, :], Y[3][3, :], title="Three dimensional Embedding for " * name))
+#     # scatter3d(Y[3][1,:], Y[3][2,:], Y[3][3,:], camera=[0,0,0])
+# end
--- Project_2/hw2.jl
+++ Project_2/hw2.jl
@@ -1,24 +1,41 @@
 
 using Pkg
 Pkg.activate("p2")
+using StatsBase
 # Pkg.instantiate()
 using DelimitedFiles
 using LinearAlgebra
 using Plots
 using LaTeXStrings
-# using JuMP
 using Convex
-# using SCS
-# using GLPK
 using CSDP
+using JLD
+using ProgressBars
+using CurveFit
 
+GHW1 = Dict{String,Any}("exactdist" => 0, "dist" => 0)
+for name = ("dist", "exactdist")
+    (R, num_vert_) = readdlm("Project_2/kn57Nodes1to57_" * name * ".txt", Float64, header=true)
+    num_vert = parse(Float64, num_vert_[1])
 
-(R, (nv, m)) = readdlm("Project_2/sparse/Sparse1kn57Nodes1to57_exactdist.txt", Float64, header=true)
-n = parse(Int64, nv)
-m = parse(Int64, m)
+    S = R .^ 2
+
+    one_col = ones(57, 1)
+    rho = 1 / (2num_vert) * one_col' * S * one_col
+    rho = rho[1]
+
+    v = ((S - rho * I(57)) * one_col) / num_vert
+    function getGram(n::Real, S::Matrix, rho::Real)::Matrix
+        r = 1 / 2n * (S - rho * I) * one_col * one_col' + 1 / 2n * one_col * one_col' * (S - rho * I) - 1 / 2 * S
+        @assert issymmetric(r)
+        return r
+    end
+    GHW1[name] = getGram(num_vert, S, rho)
+end
+
 
 """
-    evector(n, i, j)
+evector(n, i, j)
 
 Return vector e in `R^n` with +1 at `i`, -1 at `j`, and 0 else.
 """
@@ -28,41 +45,99 @@ function evector(n::Int64, i::Int64, j::Int64)::Vector{Int64}
     r[j] = -1
     return r
 end
-function matrixm(n::Int64, i::Int64, j::Int64)::Matrix{Int64}
-    e = evector(n, i, j)
-    return e * e'
-end
-eij = evector.(57, 1:57, 1:57)
-M = eij * eij'
-function create_matrix_r(R::Matrix{<:Real})::Tuple{Matrix{<:Real},Any}
+
+function create_square_dist_matrix(R::Matrix{<:Real})::Tuple{Matrix{<:Real},Any}
     dict1 = Dict(eachrow(R[:, 1:2]) .=> R[:, 3])
     matrixR = zeros(Float64, 57, 57)
     for x in dict1
         i = trunc.(Int, x[1])
         matrixR[i[1], i[2]] = x[2]
     end
-    return matrixR, [trunc.(Int, x) for x in keys(dict1)]
+    return matrixR .^ 2, [trunc.(Int, x) for x in keys(dict1)]
 end
-d, k1 = create_matrix_r(R)
 
-f1(A::Matrix{<:Real}, B::Matrix{<:Real})::Real = tr(A'B)
-f1(A::Matrix{<:Real}, B::Variable) = tr(A'B)
+files = readdir("Project_2/sparse")
+files = joinpath.("Project_2/sparse", files)
+"""
 
-f2(A::Vector{<:Real}, B::Matrix{<:Real})::Real = (A' * B * A)
-f2(A::Vector{<:Real}, B::Variable) = (A' * B * A)
+Process file and return squared distances d with indices k
+"""
+function process_file(fn::String)
+    (R, (nv, m)) = readdlm(fn, Float64, header=true)
+    # n = parse(Int64, nv)
+    m = parse(Int64, m)
+    return create_square_dist_matrix(R)
+end
 
-G = Semidefinite(57)
-# add_constraint!(G, G ≥ 0)
-# add_constraint!(G, G == G')
-# add_constraint!(G, G * ones(57) <= 0.001)
-problem = minimize(tr(G))
-er1 = 0.1
-# c1 = [abs(f1(matrixm(57, trunc(Int, i[1]), trunc(Int, i[2])), G) - d[trunc(Int, i[1]), trunc(Int, i[2])]^2) ≤ er1 for i in k1]
-# c1 = [abs(f2(evector(57, trunc(Int, i[1]), trunc(Int, i[2])), G) - d[trunc(Int, i[1]), trunc(Int, i[2])]^2) ≤ er1 for i in k1]
-c1 = [abs(tr(evector(57, i[1], i[2])' * G * evector(57, i[1], i[2])) - (d[i[1], i[2]])^2) ≤ er1 for i in k1]
-# cross(a, v::Variable) = cross()
-# c1 = [abs(cross(evector(57, trunc(Int, i[1]), trunc(Int, i[2])), G) - d[trunc(Int, i[1]), trunc(Int, i[2])]^2) ≤ er1 for i in k1]
-problem.constraints += c1
+noisy_files = last(files, 10)
+noisy_files = vcat(noisy_files[2:end], noisy_files[1])
 
-solve!(problem, CSDP.Optimizer)
-# solve!(problem, GLPK.Optimizer)
+true_files = first(files, 10)
+true_files = vcat(true_files[2:end], true_files[1])
+
+
+
+process_file(files[1])
+k = 1
+
+G_est::Dict{Real,Dict{Any,Any}} = Dict([0.1 => Dict([]), 1 => Dict([])])
+
+"""
+G_est = {
+    "0.1": {
+        "noisy10": G
+    }
+}
+G_est[0.1][noisy10]
+"""
+
+
+for f in vcat(true_files, noisy_files)
+    d, k1 = process_file(f)
+    for er1 in [0.1, 1]
+        G = Semidefinite(57)
+        problem = minimize(tr(G))
+        c1 = [abs(tr(evector(57, i[1], i[2])' * G * evector(57, i[1], i[2])) - (d[i[1], i[2]])) ≤ er1 for i in k1]
+        problem.constraints += c1
+
+        solve!(problem, CSDP.Optimizer, warmstart=true)
+        G_est[er1][f] = G.value
+    end
+end
+# @save "./G_est_dict" G_est
+# G_est = load("./G_est_dict")["G_est"]
+
+Error::Dict{Real,Vector} = Dict([0.1 => ones(10), 1 => ones(10)])
+Error_Noisy::Dict{Real,Vector} = Dict([0.1 => ones(10), 1 => ones(10)])
+for er1 = [0.1, 1]
+    k = 1
+    for f in ProgressBar(true_files)
+        Error[er1][k] = norm(GHW1["exactdist"] - G_est[er1][f])
+        k += 1
+    end
+    k = 1
+    for f in noisy_files
+        Error_Noisy[er1][k] = norm(GHW1["dist"] - G_est[er1][f])
+        k += 1
+    end
+end
+
+display(plot(range(1, 10, 10), [Error[0.1] Error_Noisy[0.1]], title=L"\epsilon=.1", label=["True Data" "Noisy Data"], xlabel="Increments of m", ylabel="Error"))
+display(plot(range(1, 10, 10), [Error[1] Error_Noisy[1]], title=L"\epsilon=1", label=["True Data" "Noisy Data"], xlabel="Increments of m", ylabel="Error"))
+
+m(x) = (x / 10) * 57 * (56) / 2
+for ep = [0.1 1]
+    b_ = (log(Error[ep][end]) - log(Error[ep][1])) / (m(10) - m(1))
+    a_ = Error[ep][1] / exp(b_ * m(1))
+    pf2(x) = a_ * exp(b_ * x)
+    display(plot(1:10, [(Error[ep]) (pf2.(m.(range(1, 10))))], title=L"\epsilon=%$(ep) \quad Error\sim e^{-mb}", label=["Error" "Error Estimation"]))
+
+    c_ = exp((log(m(10), E[ep][10]) - log(m(1), E[ep][1])) / (1 / log(m(10)) - 1 / log(m(1))))
+    d_ = -1 * log(m(1), Error[ep][1] / c_)
+    pf3(x) = c_ * x^(-1 * d_)
+    display(plot(1:10, [(Error[ep]) (pf3.(m.(1:10)))], title=L"\epsilon=%$(ep) \quad Error\sim \frac{1}{m^{\alpha}}", label=["Error" "Error Estimation"]))
+end
+
+
+G_est[1.0][files[end]]
+
