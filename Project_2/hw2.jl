fbab66d52bcf04c9c3b53ac6d761493c668e598d
Camilo Velez <camilov61@gmail.com>
2023-04-17 21:18:31 -0400

    added hwe23r1234rq

--- Project_2/hw2.jl
+++ Project_2/hw2.jl
@@ -1,15 +1,16 @@
 
 using Pkg
 Pkg.activate("p2")
-Pkg.instantiate()
+# Pkg.instantiate()
 using DelimitedFiles
 using LinearAlgebra
 using Plots
 using LaTeXStrings
-using JuMP
+# using JuMP
 using Convex
-using SCS
-using GLPK
+# using SCS
+# using GLPK
+using CSDP
 
 
 (R, (nv, m)) = readdlm("Project_2/sparse/Sparse1kn57Nodes1to57_exactdist.txt", Float64, header=true)
@@ -33,14 +34,14 @@ function matrixm(n::Int64, i::Int64, j::Int64)::Matrix{Int64}
 end
 eij = evector.(57, 1:57, 1:57)
 M = eij * eij'
-function create_matrix_r(R::Matrix{<:Real})::Tuple{Matrix{<:Real},Base.KeySet}
+function create_matrix_r(R::Matrix{<:Real})::Tuple{Matrix{<:Real},Any}
     dict1 = Dict(eachrow(R[:, 1:2]) .=> R[:, 3])
     matrixR = zeros(Float64, 57, 57)
     for x in dict1
         i = trunc.(Int, x[1])
         matrixR[i[1], i[2]] = x[2]
     end
-    return matrixR, keys(dict1)
+    return matrixR, [trunc.(Int, x) for x in keys(dict1)]
 end
 d, k1 = create_matrix_r(R)
 
@@ -50,15 +51,18 @@ f1(A::Matrix{<:Real}, B::Variable) = tr(A'B)
 f2(A::Vector{<:Real}, B::Matrix{<:Real})::Real = (A' * B * A)
 f2(A::Vector{<:Real}, B::Variable) = (A' * B * A)
 
-G = Variable(57, 57)
-add_constraint!(G, G ≥ 0)
-add_constraint!(G, G == G')
-add_constraint!(G, G * ones(57) == 0)
+G = Semidefinite(57)
+# add_constraint!(G, G ≥ 0)
+# add_constraint!(G, G == G')
+# add_constraint!(G, G * ones(57) <= 0.001)
 problem = minimize(tr(G))
-er1 = 1
-c1 = [abs(f1(matrixm(57, trunc(Int, i[1]), trunc(Int, i[2])), G) - d[trunc(Int, i[1]), trunc(Int, i[2])]^2) ≤ er1 for i in k1]
+er1 = 0.1
+# c1 = [abs(f1(matrixm(57, trunc(Int, i[1]), trunc(Int, i[2])), G) - d[trunc(Int, i[1]), trunc(Int, i[2])]^2) ≤ er1 for i in k1]
 # c1 = [abs(f2(evector(57, trunc(Int, i[1]), trunc(Int, i[2])), G) - d[trunc(Int, i[1]), trunc(Int, i[2])]^2) ≤ er1 for i in k1]
+c1 = [abs(tr(evector(57, i[1], i[2])' * G * evector(57, i[1], i[2])) - (d[i[1], i[2]])^2) ≤ er1 for i in k1]
+# cross(a, v::Variable) = cross()
+# c1 = [abs(cross(evector(57, trunc(Int, i[1]), trunc(Int, i[2])), G) - d[trunc(Int, i[1]), trunc(Int, i[2])]^2) ≤ er1 for i in k1]
 problem.constraints += c1
 
-solve!(problem, SCS.Optimizer)
+solve!(problem, CSDP.Optimizer)
 # solve!(problem, GLPK.Optimizer)
