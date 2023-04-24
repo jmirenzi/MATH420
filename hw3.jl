39a1ddca45c78d69e4553ca7bcde7422ed00b48e
Camilo Velez <camilov61@gmail.com>
2023-04-24 10:40:03 -0400

    started hw3

---
+++ hw3.jl
@@ -0,0 +1,76 @@
+using LinearAlgebra
+using Plots
+using DelimitedFiles
+
+files = readdir("Project_2/kn57Nodes1to57_coord/")
+files = joinpath.("Project_2/kn57Nodes1to57_coord/", files)
+
+# target = filter(x -> contains(x, "/Cloud"), files)
+target = files[1]
+sources = files[2:end]
+
+
+"""
+Process file and return matrix
+"""
+function process_file(fn::String)
+    m = readdlm(fn, Float64, header=false)
+    # n = parse(Int64, nv)
+    return m
+end
+
+target = process_file(target)
+sources = process_file.(sources)
+
+function compute_center(m::Matrix{Float64})::Vector{Float64}
+    n = size(m)[2]
+    r = 1 / n * m * ones(n)
+    return r
+end
+
+function recenter(m::Matrix{Float64})::Matrix{Float64}
+    n = size(m)[2]
+    m̄ = compute_center(m)
+    r = m - m̄ * ones(n)'
+    return r
+end
+function recenter(m::Matrix{Float64}, bar::Vector{Float64})::Matrix{Float64}
+    n = size(m)[2]
+    m̄ = bar
+    r = m - m̄ * ones(n)'
+    return r
+end
+
+function compute_r(x::Matrix{Float64}, y::Matrix{Float64}; center::Bool=true)::Matrix{Float64}
+    if center
+        r = recenter(x) * recenter(y)'
+    else
+        r = x * y'
+    end
+    return r
+end
+
+function compute_svd(x::Matrix{Float64})::NTuple{3,Matrix{Float64}}
+    s = svd(x)
+    r = (s.U, diagm(s.S), s.Vt)
+    return r
+end
+
+"""
+
+    Usage:
+    Q, a, z = compute_qaz(sources[i], target)
+"""
+function compute_qaz(x::Matrix{Float64}, y::Matrix{Float64})::Tuple{Matrix{Float64},Float64,Vector{Float64}}
+    x̄ = compute_center(x)
+    ȳ = compute_center(y)
+    X̃ = recenter(x, x̄)
+    Ỹ = recenter(y, ȳ)
+    u, s, vt = compute_svd(compute_r(X̃, Ỹ))
+    Q = vt' * u'
+    a = tr(s) / (norm(recenter(X̃)))^2
+    z = x̄ - 1 / a * Q' * ȳ
+    return (Q, a, z)
+end
+
+
