a7a267b1a27cad5fa376f0f089953bf86e1c36bf
Jack Mirenzi <jmirenzi717@gmail.com>
2023-04-26 23:27:37 -0400

    added matlab

--- Project_2/final_project.jl
+++ Project_2/final_project.jl
@@ -7,7 +7,9 @@ using CSDP
 using SCS
 using SDPA
 using SDPAFamily
-
+using MATLAB    
+using SDPT3 
+using JuMP
 
 function create_square_dist_matrix(R::Matrix{<:Real})::Tuple{Matrix{<:Real},Any}
     dict1 = Dict(eachrow(R[:, 1:2]) .=> R[:, 3])
@@ -31,6 +33,42 @@ function evector(n::Int64, i::Int64, j::Int64)::Vector{Int64}
     return r
 end;
 
+function compute_center(m::Matrix{Float64})::Vector{Float64}
+    n = size(m)[2]
+    r = 1 / n * m * ones(n)
+    return r
+end;
+
+function recenter(m::Matrix{Float64})::Matrix{Float64}
+    n = size(m)[2]
+    m̄ = compute_center(m)
+    r = m - m̄ * ones(n)'
+    return r
+end;
+
+function recenter(m::Matrix{Float64}, bar::Vector{Float64})::Matrix{Float64}
+    n = size(m)[2]
+    m̄ = bar
+    r = m - m̄ * ones(n)'
+    return r
+end;
+
+
+function compute_r(x::Matrix{Float64}, y::Matrix{Float64}; center::Bool=true)::Matrix{Float64}
+    if center
+        r = recenter(x) * recenter(y)'
+    else
+        r = x * y'
+    end
+    return r
+end;
+
+
+function compute_svd(x::Matrix{Float64})::NTuple{3,Matrix{Float64}}
+    s = svd(x)
+    r = (s.U, diagm(s.S), s.Vt)
+    return r
+end;
 """
 
     Usage:
@@ -46,6 +84,16 @@ function compute_qaz(x::Matrix{Float64}, y::Matrix{Float64})::Tuple{Matrix{Float
     a = tr(s) / (norm(recenter(X̃)))^2
     z = x̄ - 1 / a * Q' * ȳ
     return (Q, a, z)
+end;
+
+function compute_alignment_error(x::T, y::T, Q::T, a::Float64, z::Vector{Float64})::Float64 where {T<:Matrix{Float64}}
+    m = a * Q * (x - z * ones(size(x)[2])') - y
+    return norm(m)
+end;
+
+function compute_alignment_error(x::Matrix{Float64}, y::Matrix{Float64})::Float64
+    Q, a, z = compute_qaz(x, y)
+    return compute_alignment_error(x, y, Q, a, z)
 end;
 
 files = readdir("Project_2/data/");
@@ -76,8 +124,9 @@ iter = 1
         # solve!(problem, CSDP.Optimizer)
         # solve!(problem, SCS.Optimizer)
         
-        SDPASolver(Mode=PARAMETER_UNSTABLE_BUT_FAST);
-        solve!(problem, SDPA.Optimizer)
+        # SDPASolver(Mode=PARAMETER_UNSTABLE_BUT_FAST);
+        # solve!(problem, SDPA.Optimizer)
+        solve!(problem, SDPT3.Optimizer)
         # solve!(problem, SDPAFamily.Optimizer(presolve=false))
         G[iter] = G_.value;
     # end
@@ -86,6 +135,16 @@ iter = 1
     Q_1 = Q[:, 1:3];
     Λ_1 = Λ[1:3, 1:3];
     Y = Λ_1^1 / 2 * Q_1';
+    min_error=0;
+    for iter_2 = 1:3
+        target_ = target[iter_2];
+        target_data = readdlm(target_, Float64, header=false);
+        align_error = compute_alignment_error(copy(transpose(Y)), target_data)
+        if align_error<min_error || min_error==0
+            min_error=align_error;
+            matching_target = iter_2;
+        end
+    end
 # end
 
 
